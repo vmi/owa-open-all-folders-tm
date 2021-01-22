5c19d457b34a372169b5b3aa3a9895ac66a0c2cb
Motonori IWAMURO <vmi@nifty.com>
2021-01-22 09:59:51 +0900

    fixed the process of adding an "Open All Folders" button.

--- owa-open-all-folders-tm.js
+++ owa-open-all-folders-tm.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Open All Folders
 // @namespace    http://vmix.dev/
-// @version      1.0
+// @version      1.1
 // @description  Add "Open All Folders" button on OWA for Tampermonkey.
 // @author       vmi@nifty.com
 // @match        https://outlook.office.com/mail/*
@@ -32,19 +32,29 @@
     } else {
       console.log('Done.');
     }
-  };
+  }
 
   function __oaf_clickOpenAllFoldersButton() {
     __oaf_openAllFolders();
   }
 
-  function __oaf_addOpenAllFoldersButton() {
-    const r = document.evaluate(newMessageLocation, document.body, null, 9, null);
+  function __oaf_addOpenAllFoldersButtonInternal() {
+    var r = null;
+    try {
+      r = document.evaluate(newMessageLocation, document.body, null, 9, null);
+    } catch(e) {
+      return false;
+    }
     const nm4 = r.singleNodeValue; // span
+    if (!nm4) return false;
     const nm3 = nm4.parentElement; // span
+    if (!nm3) return false;
     const nm2 = nm3.parentElement; // span
+    if (!nm2) return false;
     const nm1 = nm2.parentElement; // button
+    if (!nm1) return false;
     const div = nm1.parentElement; // div
+    if (!div) return false;
     const b1 = document.createElement('button');
     const b2 = document.createElement('span');
     const b3 = document.createElement('span');
@@ -59,7 +69,19 @@
     b2.appendChild(b3);
     b1.appendChild(b2);
     div.appendChild(b1);
-  };
+    return true;
+  }
+
+  var retries = 0;
+  const max_retries = 300;
+
+  function __oaf_addOpenAllFoldersButton() {
+    if (!__oaf_addOpenAllFoldersButtonInternal() && retries <= max_retries) {
+      retries++;
+      console.log(`Failed to add "Open All Folders" button. Retry: ${retries}/${max_retries}`);
+      window.setTimeout(__oaf_addOpenAllFoldersButton, 1000);
+    }
+  }
 
   window.addEventListener('load', __oaf_addOpenAllFoldersButton, false);
 })();
